890d83db84039405ab598c2da672870adb03951a
Niklas <nskf2000@gmail.com>
2020-03-25 01:05:38 +0100

    Anfang der REST-Api

---
+++ controller/api-const.php
@@ -0,0 +1,20 @@
+<?php
+// Request Parameter Names
+define("ACTION", "action");
+
+// GET Requests
+define("A_UPDATE", "action_update");
+
+// POST Requests
+define("A_ENTER_GAME", "action_enter_game");
+define("A_CHECK", "action_check");
+define("A_BET", "action_bet");
+define("A_CALL", "action_call");
+define("A_RAISE", "action_raise");
+define("A_FOLD", "action_fold");
+define("A_EXIT_GAME", "action_exit-game");
+
+// Request Standard Results
+define("R_ERROR", "result_error");
+define("R_EMPTY", "result_empty");
+define("R_OK", "result_ok");
--- controller/rest.php
+++ controller/rest.php
@@ -1,4 +1,6 @@
 <?php
+include "api-const.php";
+include "action.php";
 
 switch ($_SERVER["REQUEST_METHOD"]) {
 	case "GET":
@@ -17,12 +19,46 @@ switch ($_SERVER["REQUEST_METHOD"]) {
 
 function get()
 {
-
+	$action = $_GET[ACTION];
+	if ($action == A_UPDATE) {
+		$result = update();
+	} else {
+		$result = R_ERROR;
+	}
+	makeOutput($result);
 }
 
 function post()
 {
+	$action = $_POST[ACTION];
 
+	switch ($action) {
+		case A_ENTER_GAME:
+			$result = enterGame();
+			break;
+		case A_CHECK:
+			$result = check();
+			break;
+		case A_BET:
+			$result = bet();
+			break;
+		case A_CALL:
+			$result = call();
+			break;
+		case A_RAISE:
+			$result = raise();
+			break;
+		case A_FOLD:
+			$result = fold();
+			break;
+		case A_EXIT_GAME:
+			$result = exitGame();
+			break;
+		default:
+			$result = R_ERROR;
+	}
+
+	makeOutput($result);
 }
 
 function put()
@@ -33,4 +69,9 @@ function put()
 function delete()
 {
 
+}
+
+function makeOutput($output)
+{
+	echo $output;
 }
